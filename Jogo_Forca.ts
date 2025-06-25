736f973aafd60256278c6f13faad1b124f51c3d4
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2025-06-25 16:57:42 -0300

    Adicionado uma frase e verificação do tamanho da lista de palavras

--- Jogo_Forca.ts
+++ Jogo_Forca.ts
@@ -1,5 +1,7 @@
 function forca(listaPalavras: string[], nomeEncontrado: string): string {
+     console.log("Sorteando uma palavra...");
      
+     palavras.length(listaPalavras);    
 }
 
 const palavras = ["typescript", "python", "javascript", "minecraft", "giovanna"];
